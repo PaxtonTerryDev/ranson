a4efca08d8a29d0d0a121ee94fbb632ef6819ddf
PaxtonTerryDev <paxtont@utahcounty.gov>
2024-06-06 13:49:52 -0600

    default 'UserData' class that will ship with build.

---
+++ src/classes/userData.ts
@@ -0,0 +1,32 @@
+import { PhoneFormat } from "../data/phone";
+import { firstName } from "../data/firstName";
+import { lastName } from "../data/lastName";
+import { States, USLocation } from "../data/location";
+import { Ranson, RansonData } from "./ranson";
+
+export class UserData extends RansonData {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  dob: Date;
+  location: USLocation;
+
+  constructor() {
+    super();
+    this.firstName = Ranson.generateFromDataset(firstName);
+    this.lastName = Ranson.generateFromDataset(lastName);
+    this.phone = Ranson.generateRandomUSPhone(PhoneFormat.Dashes);
+    this.email = Ranson.generateRandomEmail();
+    this.dob = Ranson.generateRandomDOB(18, 65);
+    this.location = Ranson.generateRandomUSLocation([
+      States.Utah,
+      States.NewYork,
+      States.Alabama,
+    ]);
+  }
+
+  generate(): RansonData {
+    return new UserData();
+  }
+}
