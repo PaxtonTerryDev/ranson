333fb999900139564a0e365ce99718219402d74b
PaxtonTerryDev <paxtont@utahcounty.gov>
2024-06-06 13:48:31 -0600

    configured file exports from index.js

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Ranson, RansonData } from "./classes/ranson";
+export { UserData } from "./classes/userData";
+export { USLocation, USLocationData, States } from "./data/location";
+export { PhoneFormat } from "./data/phone";
